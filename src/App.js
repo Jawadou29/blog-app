fe847e7099fb7d995e7c2659bd760e26926caa1d
jawadou29 <jawadouhdou30@gmail.com>
2024-09-27 11:18:26 +0100

    add

--- src/App.js
+++ src/App.js
@@ -22,39 +22,42 @@ import NotFound from "./pages/not-found/NotFound";
 import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { useSelector } from "react-redux";
+import { HashRouter as Router} from "react-router-dom";
 import VerifyEmail from "./pages/verify-email/VerifyEmail";
 
 function App() {
   const { user } = useSelector((state) => state.auth);
   return (
-    <BrowserRouter basename="/blog-app">
-      <ToastContainer theme="colored" position="top-center"/>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
-        <Route path="/register" element={!user ? <Register /> : <Navigate to="/" /> }/>
-        <Route path="/users/:userId/verify/:token" element={!user ? <VerifyEmail /> : <Navigate to="/" /> }/>
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:userId/:token" element={<ResetPassword />} />
-        <Route path="/profile/:id" element={<Profile />} />
-        <Route path="posts">
-          <Route index element={<PostsPage />} />
-          <Route path="create-post" element={user ? <CreatePost /> : <Navigate to="/" />} />
-          <Route path="details/:id" element={<PostDetails />} />
-          <Route path="categories/:category" element={<Category />} />
-        </Route>
-        <Route path="admin-dashboard">
-          <Route index element={user?.isAdmin ? <AdminDashboard /> : <Navigate to="/" />} />
-          <Route path="users-table" element={user?.isAdmin ? <UsersTable /> : <Navigate to="/" />} />
-          <Route path="posts-table" element={user?.isAdmin ? <PostsTable /> : <Navigate to="/" />} />
-          <Route path="categories-table" element={user?.isAdmin ? <CategoriesTable /> : <Navigate to="/" />} />
-          <Route path="comments-table" element={user?.isAdmin ? <CommentsTable /> : <Navigate to="/" />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <Router basename="/blog-app">
+      <BrowserRouter>
+        <ToastContainer theme="colored" position="top-center"/>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+          <Route path="/register" element={!user ? <Register /> : <Navigate to="/" /> }/>
+          <Route path="/users/:userId/verify/:token" element={!user ? <VerifyEmail /> : <Navigate to="/" /> }/>
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:userId/:token" element={<ResetPassword />} />
+          <Route path="/profile/:id" element={<Profile />} />
+          <Route path="posts">
+            <Route index element={<PostsPage />} />
+            <Route path="create-post" element={user ? <CreatePost /> : <Navigate to="/" />} />
+            <Route path="details/:id" element={<PostDetails />} />
+            <Route path="categories/:category" element={<Category />} />
+          </Route>
+          <Route path="admin-dashboard">
+            <Route index element={user?.isAdmin ? <AdminDashboard /> : <Navigate to="/" />} />
+            <Route path="users-table" element={user?.isAdmin ? <UsersTable /> : <Navigate to="/" />} />
+            <Route path="posts-table" element={user?.isAdmin ? <PostsTable /> : <Navigate to="/" />} />
+            <Route path="categories-table" element={user?.isAdmin ? <CategoriesTable /> : <Navigate to="/" />} />
+            <Route path="comments-table" element={user?.isAdmin ? <CommentsTable /> : <Navigate to="/" />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </Router>
   );
 }
 
